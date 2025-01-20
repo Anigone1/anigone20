4b4712f8f463e20a045a57a6f1f3f855ac515c57
Anigone1 <support@afabra.in>
2025-01-21 02:44:23 +0600

    Update layout.tsx

--- src/app/(main)/watch/[episodeId]/layout.tsx
+++ src/app/(main)/watch/[episodeId]/layout.tsx
@@ -21,12 +21,12 @@ export async function generateMetadata(
   const desc: string = description.slice(0, 146) + "...";
 
   return {
-    title: `Watching ${name}` + " | AniFire",
+    title: `Watching ${name}` + " | AniGone",
     description: desc,
     keywords: name.split(" "),
     openGraph: {
       description: desc,
-      title: `Watching ${name}` + " | AniFire",
+      title: `Watching ${name}` + " | AniGone",
       images: poster ? poster : previousImages,
     },
   };
